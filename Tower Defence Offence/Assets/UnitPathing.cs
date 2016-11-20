52acf8cc86b8900fd328e5d8bd991ebb55ee6ae9
Henry Stone <henryvstone@gmail.com>
2016-11-19 21:20:48 -0500

    path following

---
+++ Tower Defence Offence/Assets/UnitPathing.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitPathing : MonoBehaviour {
+    public Vector3[] pathPoints;
+    public double speed;
+
+    public int lastPathPoint = 0;
+    //0-1 how far along on the path between two points you are
+    public double dist = 0;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = pathPoints[lastPathPoint];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        double distanceToTravel = speed * Time.deltaTime; 
+        Vector3 lastPoint = pathPoints[lastPathPoint];
+        Vector3 nextPoint = pathPoints[lastPathPoint + 1];
+	}
+}
