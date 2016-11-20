8afe2621866b9f6cf866ab2ac6f9c2ce85c605c2
Brendan Le <le.brendan@gmail.com>
2016-11-20 02:34:05 -0500

    Stretch road sprites between nodes

--- Assets/Path Generation/GeneratePath.cs
+++ Assets/Path Generation/GeneratePath.cs
@@ -1,1 +1,34 @@
-﻿using UnityEngine;using System.Collections;public class GeneratePath : MonoBehaviour {    public GameObject[] nodes;    public GameObject thingyToConnect;	// Use this for initialization	void Start () {        for(int i = 1; i < nodes.Length; i++)        {            GameObject created = Instantiate(thingyToConnect);            created.transform.position = nodes[i].transform.position;        }	}		// Update is called once per frame	void Update () {		}}
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratePath : MonoBehaviour {
+	public GameObject[] nodes;
+	public GameObject roadSprite;
+
+	// Use this for initialization
+	void Start () {
+
+		// Stretches each path sprite between nodes
+		for(int i = 1; i < nodes.Length; i++) {
+			GameObject pathSection = Instantiate(roadSprite);
+
+			Vector3 currentPos = nodes[i].transform.position;
+			Vector3 previousPos = nodes[i-1].transform.position;
+
+			Vector3 direction = currentPos - previousPos;
+			direction.Normalize();
+			pathSection.transform.right = direction;
+
+			Vector3 centerPos = (currentPos + previousPos) / 2;
+			pathSection.transform.position = centerPos;
+
+			float distance = Vector3.Distance(currentPos, previousPos);
+			pathSection.transform.localScale = new Vector3(2 * distance, 1, 1);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
