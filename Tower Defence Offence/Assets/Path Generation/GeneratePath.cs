9d3133be8d0cd0a6f13844c4cd261a07ea08fbbb
Henry Stone <henryvstone@gmail.com>
2016-11-19 20:49:05 -0500

    nodes and blocky things

---
+++ Tower Defence Offence/Assets/Path Generation/GeneratePath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratePath : MonoBehaviour {
+    public GameObject[] nodes;
+    public GameObject thingyToConnect;
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 1; i < nodes.Length; i++)
+        {
+            GameObject created = Instantiate(thingyToConnect);
+            created.transform.position = nodes[i].transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
