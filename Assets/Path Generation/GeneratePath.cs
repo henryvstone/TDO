e0714beecdab421415c32cf6de52eac8438c6472
henryvstone <henryvstone@gmail.com>
2016-12-09 20:32:30 -0500

    unit pathing

--- Assets/Path Generation/GeneratePath.cs
+++ Assets/Path Generation/GeneratePath.cs
@@ -5,12 +5,20 @@ public class GeneratePath : MonoBehaviour {
 	public GameObject[] nodes;
 	public GameObject roadSprite;
 
+	public Vector3[] points;
+
+
+
 	// Use this for initialization
 	void Start () {
 
+		points = new Vector3[nodes.Length];
+		points [0] = nodes [0].transform.position;
 		// Stretches each path sprite between nodes
 		for(int i = 1; i < nodes.Length; i++) {
+			points [i] = nodes [i].transform.position;
 			GameObject pathSection = Instantiate(roadSprite);
+			pathSection.transform.parent = this.transform;
 
 			Vector3 currentPos = nodes[i].transform.position;
 			Vector3 previousPos = nodes[i-1].transform.position;
@@ -23,10 +31,14 @@ public class GeneratePath : MonoBehaviour {
 			pathSection.transform.position = centerPos;
 
 			float distance = Vector3.Distance(currentPos, previousPos);
-			pathSection.transform.localScale = new Vector3(2 * distance, 1, 1);
-		}
-	}
+			pathSection.transform.localScale = new Vector3(distance, 1, 1);
 
+			DestroyObject (nodes [i]);
+		}
+
+		DestroyObject (nodes [0]);
+	}
+		
 	// Update is called once per frame
 	void Update () {
 
--- Assets/UnitPathing.cs
+++ Assets/UnitPathing.cs
@@ -2,22 +2,51 @@
 using System.Collections;
 
 public class UnitPathing : MonoBehaviour {
-    public Vector3[] pathPoints;
-    public double speed;
+    private Vector3[] pathPoints;
+	public GeneratePath path;
 
+    public float speed = 1;
     public int lastPathPoint = 0;
+	private Vector3 lastPoint;
+	private Vector3 nextPoint;
+	private float distanceBetween;
     //0-1 how far along on the path between two points you are
-    public double dist = 0;
+    public float distAlong = 0;
 
 	// Use this for initialization
 	void Start () {
-        transform.position = pathPoints[lastPathPoint];
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        double distanceToTravel = speed * Time.deltaTime; 
-        Vector3 lastPoint = pathPoints[lastPathPoint];
-        Vector3 nextPoint = pathPoints[lastPathPoint + 1];
+		if (pathPoints == null) {
+			pathPoints = path.points;
+			transform.position = pathPoints[lastPathPoint];
+			lastPoint = pathPoints [lastPathPoint];
+			nextPoint = pathPoints [lastPathPoint + 1];
+			distanceBetween = Vector3.Distance (lastPoint, nextPoint);
+		} else {
+			float distanceToTravel = speed * Time.deltaTime; 
+
+			while (distanceToTravel + distAlong > distanceBetween) {
+				distanceToTravel -= distanceBetween - distAlong;
+				lastPathPoint++;
+				if (lastPathPoint == pathPoints.Length - 1) {
+					lastPathPoint = 0;
+				}
+				lastPoint = pathPoints[lastPathPoint];
+				nextPoint = pathPoints[lastPathPoint + 1];
+				distanceBetween = getDistance (lastPoint, nextPoint);
+				distAlong = 0;
+			}
+			distAlong += distanceToTravel;
+
+			this.transform.position = (distAlong * nextPoint + (distanceBetween - distAlong) * lastPoint) / distanceBetween;
+		}
+	}
+
+	private float getDistance(Vector3 v1, Vector3 v2){
+		return Vector3.Distance (v1, v2);
 	}
 }
