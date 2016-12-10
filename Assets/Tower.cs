1efa5b73fe2374cf8219c5e0e6ea55c500ac7ed3
Brendan Le <le.brendan@gmail.com>
2016-12-09 20:38:44 -0500

    Add tower functionality

---
+++ Assets/Tower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 20f;
+	public float fireRate = 1f;
+	public GameObject units;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		foreach (Transform child in units.transform) {
+			float distance = (child.position - this.transform.position).magnitude;
+			if (distance < this.range) {
+				UnitBehavior unit = child.GetComponent<UnitBehavior>();
+				unit.setHealth(unit.getHealth() - this.damage);
+			}
+		}
+	}
+}
---
+++ Assets/UnitBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitBehavior : MonoBehaviour {
+	public float maxHealth = 100;
+	private float health = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(this.health);
+	}
+
+	public float getHealth() {
+		return this.health;
+	}
+
+	public void setHealth(float health) {
+		this.health = health;
+	}
+}
